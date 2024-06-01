fee402f0fda741458d38648fb32b5faacd1deb14
Shura ASH <shuraash@gmail.com>
2024-06-01 05:35:37 +0200

    audio aka player

--- src/app/details/page.js
+++ src/app/details/page.js
@@ -26,7 +26,7 @@ export default function About() {
 
 		setDjd(shuffledDJ())
 
-		setInterval(() =>  makafaka(), 2345)
+		setInterval(() =>  makafaka(), 888*6)
 
 	}, [])
 
@@ -74,7 +74,7 @@ export default function About() {
 				{djs && djs.map((dj,pos) =>
 					<li key={dj[0]} id={'dj' + dj[0].replaceAll(' ','_')}
 						className={'absolute w-full text-center grid justify-center items-center grid-cols-[auto_auto] gap-x-3'}
-					    style={{top: (pos * 25) + 'px', transition: 'all ' + (0.5 + Math.random()/2) + 's ease-in-out'}}
+					    style={{top: (pos * 25) + 'px', transition: 'all ' + (0.888*4.5 + Math.random()/2) + 's ease-in-out'}}
 					>
 						<PsycoTexto className={'flex items-center '}><PsycoTitlo text={dj[0]} className={'uppercase text-orange-300'}/></PsycoTexto>
 						<div className={'text-neutral-300 translate-y-1'}> {dj[1]}</div>
