0673903780b09610b7a6a6d36ebbdb7f986a9261
Shura ASH <shuraash@gmail.com>
2024-06-06 23:13:06 +0200

    audio aka player

--- src/app/details/page.js
+++ src/app/details/page.js
@@ -37,8 +37,11 @@ export default function About() {
 
 		<div className={'mt-[100px] w-full max-w-screen-sm mx-auto px-4 sm:px-0 py-5  sm:bg-neutral-600/50 text-center'}>
 
-			<div className={'text-xl'}>Free party</div>
-			<div className={'text-xl text-rose-400'}>FULL TRUE LOVE AND HAPPINESS!</div>
+			<div className={'flex justify-center text-2xl'}>
+				<PsycoTexto className={'flex items-center '}><PsycoTitlo text={'* Free partY *'} className={'uppercase text-orange-300'}/></PsycoTexto>
+			</div>
+
+			{/*<div className={'text-xl text-rose-400'}>FULL TRUE LOVE AND HAPPINESS!</div>*/}
 
 			<div className={'text-xl'}>
 				<a href={'https://maps.app.goo.gl/fiCzbZd4iYYzwX5h6'} target={'_blank'} >
@@ -51,21 +54,21 @@ export default function About() {
 			<br/>
 
 
-<div>
-			WE DO NOT ALLOW:
+{/*<div>*/}
+{/*			WE DO NOT ALLOW:*/}
 
-			<ul className={"text-left pl-12 w-fit mx-auto text-sm"}>
-				<li>BAD VIBES</li>
-				<li>BAD ATTITUDES</li>
-				<li>DRUGS</li>
-				<li>PUBLICITY OF ANY KIND</li>
-				<li>LACK OF RESPECT to nature or persons</li>
-				<li>FIRE (we will provide our own firecamp for your pleasure)</li>
-				<li>CAMPING OUT OF THE CAMPING PLACE</li>
-			</ul>
-</div>
+{/*			<ul className={"text-left pl-12 w-fit mx-auto text-sm"}>*/}
+{/*				<li>BAD VIBES</li>*/}
+{/*				<li>BAD ATTITUDES</li>*/}
+{/*				<li>DRUGS</li>*/}
+{/*				<li>PUBLICITY OF ANY KIND</li>*/}
+{/*				<li>LACK OF RESPECT to nature or persons</li>*/}
+{/*				<li>FIRE (we will provide our own firecamp for your pleasure)</li>*/}
+{/*				<li>CAMPING OUT OF THE CAMPING PLACE</li>*/}
+{/*			</ul>*/}
+{/*</div>*/}
 
-			<p className={'text-xl'}>No any sales. Drinks and food bring with you.</p>
+			<p className={'text-xl'}>Will be no any sales. Drinks and food bring with you please.</p>
 
 			<div className={'pt-2.5'}>
 
