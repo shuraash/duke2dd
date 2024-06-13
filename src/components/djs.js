37f4424288ef40d24e3248f475ba0d91f4c8eb1c
Shura ASH <shuraash@gmail.com>
2024-06-13 18:34:44 +0200

    audio aka player

--- src/components/djs.js
+++ src/components/djs.js
@@ -10,7 +10,17 @@ export const djs = [
 	["Hyperactive Mode", "Antwerp.BE"],
 	["Mechanical Trip", "Boevange.LU"],
 	["PsyGogh", "Amsterdam.NL"],
-	["Frequency-F", "Esch-sur-Alzette.LU"],
+	["Frequency-F", "Esch-sur-Alzette.LU",
+<>
+	 <div className={`text-2xl text-orange-500 font-semibold text-center`}>
+		 First time!
+	 </div>
+
+	 <div className={'text-center'}>
+		 dj Psycorax and dj Fabi first time prsent they new project.
+	 </div>
+</>
+	],
 	["Narah Gaia", "Amsterdam.NL"],
 	["Shiva ASH", "Esch-sur-Alzette.LU"],
 	["Input Malfunction", "Antwerp.BE"],
