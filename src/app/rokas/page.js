ab3dd23c0e4938bfd7fd605e18fe43e446bec108
Shura ASH <shuraash@gmail.com>
2025-04-12 06:23:42 +0200

    et

---
+++ src/app/rokas/page.js
@@ -0,0 +1,27 @@
+
+export default async function Rokas({children}) {
+
+	return <body className={'w-screen h-screen bg-blend-color text-yellow-300 py-8 px-8 text-center grid grid-cols-1 gap-y-2'}>
+
+
+		<a href={'https://trancescript.ddns.net/video/rokas/r1.mp4'} target={'_blank'}>Rokas 01</a>
+		<a href={'https://trancescript.ddns.net/video/rokas/r2.mp4'} target={'_blank'}>Rokas 02</a>
+		<a href={'https://trancescript.ddns.net/video/rokas/r3.mp4'} target={'_blank'}>Rokas 03</a>
+		<a href={'https://trancescript.ddns.net/video/rokas/r4.mp4'} target={'_blank'}>Rokas 04</a>
+		<a href={'https://trancescript.ddns.net/video/rokas/r5.mp4'} target={'_blank'}>Rokas 05</a>
+		<a href={'https://trancescript.ddns.net/video/rokas/r6.mp4'} target={'_blank'}>Rokas 06</a>
+		<a href={'https://trancescript.ddns.net/video/rokas/r7.mp4'} target={'_blank'}>Rokas 07</a>
+		<a href={'https://trancescript.ddns.net/video/rokas/r8.mp4'} target={'_blank'}>Rokas 08</a>
+		<a href={'https://trancescript.ddns.net/video/rokas/r9.mp4'} target={'_blank'}>Rokas 09</a>
+
+
+		{children}
+
+	</body>
+
+}
+
+
+
+
+
